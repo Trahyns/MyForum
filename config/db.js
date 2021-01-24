120105a8d3d2060b5d27a6fa8f1fdcb2d17a753f
Toni R <trahyns@gmail.com>
2021-01-23 20:37:20 -0500

    update config

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const config = require('./default.js');
+const db = config.MONGODB_URI;
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true,
+        });
+        console.log('MongoDB Connected');
+    } catch (error) {
+        console.error(error.message);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
